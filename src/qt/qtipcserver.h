3d78cf3363ac9713f92c2b8415e36b3e25bfbd1f
zak <voxpopulidev@yahoo.fr>
2014-10-07 20:40:30 +0100

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define voxpopuli-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "voxpopuliURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
